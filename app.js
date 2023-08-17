d3b88b1e1656e997125198520b19ac8fc0046aab
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-08-17 21:19:49 +0300

    Update app.js
    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
 
-app.use('/', () => {
-    console.log('Це колбек для app.use('/')');
+// http://localhost: 4444/
+// http://localhost: 4444/about
+
+app.get('/', () => {
+    console.log('Це колбек для app.get("/")');
+})
+
+app.get('/about', () => {
+    console.log('Це колбек для app.get("/about")');    
 })
 
 app.listen(4444, () => { 
     console.log(`Application server is running on port ${4444}`);
 });
+
